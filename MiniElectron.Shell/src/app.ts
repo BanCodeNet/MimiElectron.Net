64cd60bf67eabb6a1de7247bb0145fc88abe7ea1
Smilecl <chenjiahao@xcewell.cn>
2021-11-18 10:06:04 +0800

    MOD：增加主题切换与托盘图标交互

--- MiniElectron.Shell/src/app.ts
+++ MiniElectron.Shell/src/app.ts
@@ -1,9 +1,10 @@
-import { app, BrowserWindow, dialog } from 'electron';
+import { app, BrowserWindow, Menu, Tray, nativeTheme } from 'electron';
 import * as fs from 'fs';
 import * as os from 'os';
 import * as path from 'path';
 import { spawn } from 'child_process';
 import { Bridge } from './bridge';
+//import * as ws from 'ws';
 
 const sockName = 'coreToShell'
 let sockPath: string
@@ -25,8 +26,65 @@ app.on("ready", () => {
     startCore()
 })
 
+app.on("window-all-closed", () => {
+    if (process.platform != "darwin") {
+        app.quit()
+    }
+})
+
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows.length == 0) {
+        createWindow()
+    }
+})
+
+let tray = null
+app.whenReady().then(() => {
+    tray = new Tray('desktopIcon.ico')
+    const contextMenu = Menu.buildFromTemplate([
+        {
+            label: 'exit', type: 'normal', click: function () {
+                app.quit();
+            }
+        }
+    ])
+    tray.setToolTip('My electron Application')
+    tray.setContextMenu(contextMenu)
+    tray.on('click', function () { win.show() })
+    tray.on('double-click', function () { })
+
+    // var wss = new ws("ws://121.40.165.18:8800");
+    // var a = wss.readyState;
+    // wss.onopen = function () {
+    //     var aa = wss.readyState;
+    //     console.log("open");
+    //     wss.send("hello");
+    // }
+    // wss.onmessage = function (e) {
+    //     console.log(e.data);
+    // }
+    // wss.onclose = function (e) {
+    //     console.log("close");
+    // }
+    // wss.onerror = function (e) {
+    //     console.log(e);
+    // }
+})
+
+//     var win = BrowserWindow.getFocusedWindow()
+//     win.on('close', (e) => {
+//         if (!win.isFocused()) {
+//             win = null
+//         }
+//         else {
+//             e.preventDefault()
+//             win.hide()
+//         }
+//     })
+
+let win
 const createWindow = () => {
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
         width: 1366,
         height: 1024,
         show: false,
@@ -40,6 +98,45 @@ const createWindow = () => {
         // win.webContents.openDevTools()
     })
     win.loadFile('index.html')
+    win.on('closed', () => {
+        win = null
+    })
+
+    themeMenu();
+}
+
+const themeMenu = () => {
+    const template =
+        [
+            {
+                label: 'ThemeSwitch', submenu: [
+                    {
+                        label: 'dark',
+                        click: () => {
+                            let allWins = BrowserWindow.getAllWindows();
+                            if (allWins != null && allWins.length > 0) {
+                                nativeTheme.themeSource = 'dark';
+                                //allWins.forEach(win => win.webContents.send('change_theme', 'dark'));
+                                //ipcRenderer on change_theme
+                            }
+                        }
+                    },
+                    {
+                        label: 'white',
+                        click: () => {
+                            let allWins = BrowserWindow.getAllWindows();
+                            if (allWins != null && allWins.length > 0) {
+                                nativeTheme.themeSource = 'light';
+                                //allWins.forEach(win => win.webContents.send('change_theme', 'white'));
+                                //ipcRenderer on change_theme
+                            }
+                        }
+                    }
+                ]
+            }
+        ];
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
 }
 
 const startCore = () => {
